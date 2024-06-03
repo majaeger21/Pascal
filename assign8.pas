062ba8847c78abea900b7cb5fb0cc639b9837655
Meyli Jaeger <mjaege02@calpoly.edu>
2024-06-03 13:50:48 -0700

    datatypes

--- assign8.pas
+++ assign8.pas
@@ -1,4 +1,13 @@
-Program HelloWorld(output);
-begin
-  writeln('Hello, world!');
-end.
+program assign8
+
+{Data Types }
+
+type
+NumC = record
+   n : integer 
+end;
+
+type
+StrC = record
+   s : packed array [1..50] of char;
+end;
