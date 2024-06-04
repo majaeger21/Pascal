b9886df7bea9ca5cbb8137270c5527976ebd5059
Logan Costello <logancostello2022@gmail.com>
2024-06-04 16:03:58 -0700

    interp for NumC and StrC. NumV and StrV created

--- assign8.pas
+++ assign8.pas
@@ -1,7 +1,6 @@
 program assign8;
 
-{Data Types }
-
+{Data Types}
 type
 NumC = record
    n : integer; 
@@ -9,7 +8,17 @@ end;
 
 type
 StrC = record
-   s : packed array [1..50] of char;
+   s : String;
+end;
+
+type
+NumV = record
+   n : integer; 
+end;
+
+type
+StrV = record
+   s : String;
 end;
 
 
@@ -19,6 +28,14 @@ type
     case exp: Integer of
       1: (Num: NumC);
       2: (Str: StrC);
+  end;
+  
+{Value Type}
+type
+  Val = record
+    case exp: Integer of
+      1: (Num: NumV);
+      2: (Str: StrV);
   end;
 
 { Prints what type ExprC is holding }
@@ -32,17 +49,46 @@ begin
   end;
 end;
 
+{interp}
+function interp(U : ExprC) : Val;
 var
-   E1: ExprC;
+    result : Val; {result value that will be returned}
+begin
+    case U.Exp of
+    1: {NumC case}
+        begin
+            result.exp := 1;
+            result.Num.n := U.Num.n;
+        end;
+    2: {StrC Case}
+        begin
+            result.exp := 2;
+            result.Str.s := U.Str.s;
+        end;
+    end;
+    interp := result; {return result}
+end;
 
+{regular code for testing}
 var
-   n1: NumC;
+    E1: ExprC;
+    n1: NumC;
+    E2: ExprC;
+    s2: StrC;
 
 begin
-   n1.n := 20;
+   n1.n := 30;
    E1.exp := 1;
    E1.Num := n1;
    writeln('Data type is  ', GetExp(E1));
    writeln('Attempt to print NumC, only val atm', E1.Num.n);
+   
+   s2.s := 'hello world';
+   E2.exp := 2;
+   E2.Str := s2;
+
+
+   writeln('E1 interpretted: ', interp(E1).Num.n);
+   writeln('E2 interpretted: ', interp(E2).Str.s);
 
 end.
